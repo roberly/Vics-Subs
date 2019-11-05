0777cb08b1ec37bb721842ab431d3747d4c8ada3
ryan <oberlyr@go.stockton.edu>
2019-11-05 16:12:36 -0500

    Initial

---
+++ src/sample/LoginController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+    @FXML
+    Button loginButton;
+
+    @FXML
+    public void bringUpWelcomeGui() throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Welcome.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Welcome To Vics!");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+}
---
+++ src/sample/WelcomeController.java
@@ -0,0 +1,80 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class WelcomeController {
+
+
+    /**
+     * Clock in
+     */
+    @FXML
+    public void bringUpClockInGui() throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ClockIn.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Clock In");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    /**
+     * Clock out
+     */
+    @FXML
+    public void bringUpClockOutGui() throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ClockOut.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Clock Out");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    /**
+     * Schedule
+     */
+    @FXML
+    public void bringUpScheduleGui() throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Schedule.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Employee Schedule");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    /**
+     * Hours Worked
+     */
+    @FXML
+    public void bringUpHoursWorkedGui() throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HoursWorked.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("View Hours Worked");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    /**
+     * Request off
+     */
+    @FXML
+    public void bringUpRequestOffGui() throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RequestOff.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Request Off");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+
+
+
+}
