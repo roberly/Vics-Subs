2c4894f1eb66c87e6e425621bd1ae9f12a905011
roberly <55609131+roberly@users.noreply.github.com>
2019-11-07 23:42:37 -0500

    Connected Database

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -7,22 +7,13 @@ import java.sql.SQLException;
 public class DBConnection
 {
     private Connection connection;
-    private String url = "jdbc:mysql://127.0.0.1/VicsSubs";
-    //private String url = "jdbc:mysql://localhost:3306/addressbook?useSSL=false";
+    private String url = "jdbc:mysql://localhost:3306/VicsSubs?useSSL=false";
     private String user = "root";
-    private String pass = "";
+    private String pass = "root";
 
     public Connection getConnection() throws SQLException
     {
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            connection = DriverManager.getConnection(url, user, pass);
-        }
-        catch(SQLException e)
-        {
-            System.out.println(e.getMessage());
-        }
+        connection = DriverManager.getConnection(url, user, pass);
 
         return connection;
 
