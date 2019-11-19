381adb48762b581f53b9cfa079b28c27e806e7b1
ryan <oberlyr@go.stockton.edu>
2019-11-19 15:46:28 -0500

    Fixed Rob's barbaric code spacing

--- src/View/AdminWelcomeController.java
+++ src/View/AdminWelcomeController.java
@@ -12,22 +12,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AdminWelcomeController
-
 {
     @FXML
     Button LogoutButton;
+
     @FXML
-    public void bringUpInputHoursScreen() throws IOException {
+    public void bringUpInputHoursScreen() throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("InputHoursScreen.fxml"));
         Parent root1 = (Parent) fxmlLoader.load();
         Stage stage = new Stage();
         stage.setTitle("Manually Input or Delete Hours");
         stage.setScene(new Scene(root1));
         stage.show();
-
     }
+
     @FXML
-    public void bringUpModifyEmployee() throws IOException {
+    public void bringUpModifyEmployee() throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
         Parent root1 = (Parent) fxmlLoader.load();
         Stage stage = new Stage();
@@ -36,8 +38,10 @@ public class AdminWelcomeController
         stage.show();
 
     }
+
     @FXML
-    public void bringUpViewRequestsOff() throws IOException {
+    public void bringUpViewRequestsOff() throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
         Parent root1 = (Parent) fxmlLoader.load();
         Stage stage = new Stage();
@@ -46,17 +50,19 @@ public class AdminWelcomeController
         stage.show();
 
     }
+
     @FXML
     public void bringUpModifySchedule() throws IOException
     {
-    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
-    Parent root1 = (Parent) fxmlLoader.load();
-    Stage stage = new Stage();
-    stage.setTitle("Modifying Employee");
-    stage.setScene(new Scene(root1));
-    stage.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Modifying Employee");
+        stage.setScene(new Scene(root1));
+        stage.show();
 
     }
+
     @FXML
     public void bringUpViewAllHoursWorked() throws IOException
     {
@@ -68,12 +74,11 @@ public class AdminWelcomeController
         stage.show();
 
     }
+
     @FXML
-    public void handleCloseButtonAction() {
+    public void handleCloseButtonAction()
+    {
         Stage stage = (Stage) LogoutButton.getScene().getWindow();
         stage.close();
     }
-
-
-
 }
