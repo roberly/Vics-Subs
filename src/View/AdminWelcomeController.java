1735d0e625b0fa414a01fb9733d0da98d3a98453
Rob <roberthamby1@gmail.com>
2019-11-19 15:43:58 -0500

    Fixed Obes' naming ocd

--- src/View/AdminWelcomeController.java
+++ src/View/AdminWelcomeController.java
@@ -1,14 +1,79 @@
 package View;
 
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
 public class AdminWelcomeController
+
 {
-    public void bringUpInputHoursScreen()
-    {
+    @FXML
+    Button LogoutButton;
+    @FXML
+    public void bringUpInputHoursScreen() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("InputHoursScreen.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Manually Input or Delete Hours");
+        stage.setScene(new Scene(root1));
+        stage.show();
 
     }
-    public void bringUpModifyEmployee()
-    {
+    @FXML
+    public void bringUpModifyEmployee() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Modifying Employee");
+        stage.setScene(new Scene(root1));
+        stage.show();
 
     }
+    @FXML
+    public void bringUpViewRequestsOff() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Modifying Employee");
+        stage.setScene(new Scene(root1));
+        stage.show();
+
+    }
+    @FXML
+    public void bringUpModifySchedule() throws IOException
+    {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
+    Parent root1 = (Parent) fxmlLoader.load();
+    Stage stage = new Stage();
+    stage.setTitle("Modifying Employee");
+    stage.setScene(new Scene(root1));
+    stage.show();
+
+    }
+    @FXML
+    public void bringUpViewAllHoursWorked() throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifyEmployee.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Modifying Employee");
+        stage.setScene(new Scene(root1));
+        stage.show();
+
+    }
+    @FXML
+    public void handleCloseButtonAction() {
+        Stage stage = (Stage) LogoutButton.getScene().getWindow();
+        stage.close();
+    }
+
+
 
 }
