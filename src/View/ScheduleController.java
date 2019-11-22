3289979979ee4bd25d743bb7598dee72c6b1a6c7
ryan <oberlyr@go.stockton.edu>
2019-11-22 09:26:28 -0500

    Admins can view schedules

--- src/View/ScheduleController.java
+++ src/View/ScheduleController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
