25f47a3f9f05656822454b810d789144ce287452
Don Martin <don.martin00@yahoo.com>
2019-11-26 20:27:14 -0500

    Hopefully all my work stays this time because i almost had a heart attack and dropped this class :)

--- src/View/ViewRequestsOffController.java
+++ src/View/ViewRequestsOffController.java
@@ -1,13 +1,49 @@
 package View;
 
+import Database.DBConnection;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ViewRequestsOffController {
 
+    public void initialize() throws SQLException {
+        fillTable();
+        fillOutSelectedData();
+    }
+
     @FXML
     Button doneButton;
+
+    @FXML
+    Button approveButton;
+
+    @FXML
+    Button denyButton;
+
+    @FXML
+    TableView requestsOffTableView;
+
+    @FXML
+    TextField employeeTextField;
+
+    @FXML
+    TextField startDateTextField;
+
+    @FXML
+    TextField endDateTextField;
 
     @FXML
     public void handleCloseButtonAction()
@@ -15,4 +51,67 @@ public class ViewRequestsOffController {
         Stage stage = (Stage) doneButton.getScene().getWindow();
         stage.close();
     }
+
+    DBConnection dbConnection = new DBConnection();
+
+    public void fillTable() throws SQLException {
+        Connection connection = dbConnection.getConnection();
+        ObservableList<ObservableList> requestOffInfo = FXCollections.observableArrayList();
+        String sqlData = "SELECT Employee_ID, StartDate, EndDate from RequestedTimeOff";
+        System.out.println(sqlData);
+        ResultSet resultSet = connection.createStatement().executeQuery(sqlData);
+        System.out.println(resultSet);
+
+        for (int i = 0; i<resultSet.getMetaData().getColumnCount(); i++){
+            final int j = i;
+            TableColumn column = new TableColumn(resultSet.getMetaData().getColumnName(i+1));
+            column.setCellValueFactory
+                    (new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                @Override
+                public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                    return new SimpleStringProperty(param.getValue().get(j).toString());
+                }
+            });
+
+            requestsOffTableView.getColumns().addAll(column);
+        }
+
+        while (resultSet.next()) {
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++){
+                row.add(resultSet.getString(i));
+            }
+            requestOffInfo.add(row);
+        }
+        requestsOffTableView.setItems(requestOffInfo);
+    }
+
+    public void fillOutSelectedData(){
+        requestsOffTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+        {
+            Object selectedItems = requestsOffTableView.getSelectionModel().getSelectedItems().get(0);
+            String employeeId = selectedItems.toString().split(",")[0].substring(1);
+            employeeTextField.setText(employeeId);
+            String startDate = selectedItems.toString().split(",")[1].substring(1);
+            startDateTextField.setText(startDate);
+            String endDate = selectedItems.toString().split(",")[2].substring(1);
+            endDateTextField.setText(endDate.substring(0,endDate.length()-1));
+        });
+    }
+
+    public void removeRowFromTable(){
+        Object selectedItems = requestsOffTableView.getSelectionModel().getSelectedItems().get(0);
+        requestsOffTableView.getItems().remove(selectedItems);
+    }
+
+    @FXML
+    public void deny(){
+        removeRowFromTable();
+    }
+
+    @FXML
+    public void approve(){
+
+    }
+
 }
